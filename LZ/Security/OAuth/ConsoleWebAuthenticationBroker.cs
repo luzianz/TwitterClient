089337d538b03919302a2b71c629f02fd410c11a
Luzian Zagadinow <luzianz@outlook.com>
2017-05-11 21:34:33 -0400

    Using WebListener instead of removed HttpListener

--- LZ/Security/OAuth/ConsoleWebAuthenticationBroker.cs
+++ LZ/Security/OAuth/ConsoleWebAuthenticationBroker.cs
@@ -1,14 +1,15 @@
+using Microsoft.Net.Http.Server;
 using System;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.AspNetCore.WebUtilities.QueryHelpers;
 
 namespace LZ.Security.OAuth
 {
 	public class ConsoleWebAuthenticationBroker : IWebAuthenticationBroker
 	{
 		private readonly Encoding utf8;
-		
+
 		public ConsoleWebAuthenticationBroker()
 		{
 			utf8 = System.Text.Encoding.GetEncoding(65001);
@@ -16,32 +17,30 @@ namespace LZ.Security.OAuth
 		
 		public async Task<ICredential> AuthenticateAsync(Uri requestUri, Uri callbackUri = null)
 		{
+			var settings = new WebListenerSettings();
 			var requestUriStrb = new StringBuilder();
 			requestUriStrb.Append(requestUri.AbsoluteUri);
-			
+
 			if (callbackUri != null)
 			{
+				settings.UrlPrefixes.Add(callbackUri.AbsoluteUri);
 				requestUriStrb.AppendFormat("&oauth_callback={0}", callbackUri.AbsoluteUri);
 			}
 			
-			var listener = new HttpListener();
-			listener.Prefixes.Add(callbackUri.AbsoluteUri);
-			listener.Start();
-			
-			System.Diagnostics.Process.Start(requestUriStrb.ToString());
-			
-			var ctx = await listener.GetContextAsync();
-			
-			var token = new Credential(ctx.Request.QueryString.Get("oauth_token"), ctx.Request.QueryString.Get("oauth_verifier"));
-			
-			// empty response
-			// ctx.Response.ContentLength64 = 0;
-			// await ctx.Response.OutputStream.WriteAsync(new byte[0], 0, 0);
-			ctx.Response.OutputStream.Close();
-			
-			listener.Stop();
-			
-			return token;
+			using (var listener = new WebListener(settings))
+			{
+				listener.Start();
+				
+				using (var ctx = await listener.AcceptAsync())
+				{
+					var query = ParseQuery(ctx.Request.QueryString);
+					var oauth_token = query["oauth_token"].ToString();
+					var oauth_verifier = query["oauth_verifier"].ToString();
+					var token = new Credential(oauth_token, oauth_verifier);
+
+					return token;
+				}
+			}
 		}
 
 		public Uri GetCurrentApplicationCallbackUri()
